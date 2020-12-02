2b065a0dded4a4c8153bcf5be95c5fd191cf1f55
OddieMaster <romualdo.gui@gmail.com>
2020-12-02 11:39:59 -0300

    primeirinho

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return   (
+   <div className="App">
+    <h1> CRUD APLLICATION</h1>
+    <div className="form">
+      <label>Movie Name:</label>
+    <input type="text" name="movieName"/>
+    <label>review:</label>
+    <input type="text" name="review"/>
+
+    <button>Submit</button>
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get ('/', (req, res) => {
+    res.send ("hello gui");
+});
+
+app.listen(3001, () => {
+    console.log ("running on port 3001");
+});
+

2e989d24445be82f09d025d6cd5a65edcc02ef9c
OddieMaster <romualdo.gui@gmail.com>
2020-12-04 11:30:13 -0300

    Atualizações no código

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Axios from "axios";
+var cors = require('cors');
+
 
 function App() {
 
-  const [movieName, setMovieName] = useState('')
-  const [review, setReview] = useState('') 
+ const [movieName, setMovieName] = useState('');
+ const [review, setReview] = useState('');
+
+ const submitReview = () => {
+ Axios.post('http://localhost:3001/api/insert', {
+   movieName: movieName,
+    movieReview: review,
+  }).then(() => {
+    alert("Sucessful Insert");
+  });
+ };
+
+
   return   (
    <div className="App">
     <h1> CRUD APLLICATION</h1>
     <div className="form">
-      <label>Movie Name:</label>
-    <input type="text" name="movieName"/>
-    <label>review:</label>
-    <input type="text" name="review"/>
 
-    <button>Submit</button>
+
+    <label>Movie Name:</label>
+      <input type="text" 
+        name="movieName" 
+        onChange={(e)=> {
+           setMovieName(e.target.value);
+           
+    }}/>
+
+    
+    <label>Review:</label>
+    <input type="text"
+        name="review" 
+        onChange={(e)=> {
+         setReview(e.target.value);
+    }}/>
+
+
+    <button onClick={submitReview}>Submit</button>
     </div>
   </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
+const bodyParser = require('body-parser');
+var cors = require('cors');
 
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Eduardo123!",
-    database: "cruddatabase",
+const mysql = require('mysql');
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Eduardo123!',
+    database: 'crudapp',
 });
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get ("/", (req, res) => {});
+app.post("/api/insert", (req, res) =>  {
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert=
+    "INSERT INSERT INTO movie_schema (movieName, movieReview) VALUES (?,?);"
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log (result);
+        });
+    });
+
+
+
 
 app.listen(3001, () => {
     console.log ("running on port 3001");
+
 });
 
